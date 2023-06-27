6550aaac57dbdb5f895619922e6dd7cba86f377a
iRichard <zerorichardx@gmail.com>
2023-06-27 06:35:15 -0500

    repair _app problem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "./layout";
-import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
