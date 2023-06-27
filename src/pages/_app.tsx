6538782293043de99cb0dd335ca9e7d5d7a29a3b
iRichard <zerorichardx@gmail.com>
2023-06-27 11:53:15 -0500

    added pagination

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "./layout";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-
   return (
     <Layout>
       <Component {...pageProps} />
--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -11,6 +11,7 @@ export async function getServerSideProps() {
     return {
       props: {
         resData: resData as blogdata[],
+        error:null
       },
     };
   } catch (error: any) {
@@ -18,6 +19,7 @@ export async function getServerSideProps() {
     return {
       props: {
         resData: null,
+        error:errorMessage
       },
     };
   }
@@ -25,8 +27,10 @@ export async function getServerSideProps() {
 
 export default function Blog({
   resData,
+  error
 }: {
   resData: blogdata[];
+  error:any
 }) {
   const [page, setPage] = useState(1);
   console.log(page, '💜 Work in');
@@ -53,7 +57,10 @@ export default function Blog({
         </div>
         <div className="w-full h-auto flex items-center justify-center bg-[#2C2A4A]">
         <div className="w-[90%] h-auto inline-grid place-items-center grid-cols-1 grid-rows-3">    
+          {error?<ErrorMessage error={error} />:
           <BlogSection resData={sliceData}/>       
+          }
+          <Paginate resData={resData} setPaginated={setPageHandler}/>
         </div>
         </div>
       </div>
