ce1c5be556039d47cfecee77364d818b27762917
iRichard <zerorichardx@gmail.com>
2023-06-27 11:53:15 -0500

    added pagination

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "./layout";
 import { useRouter } from "next/router";
-import {useMemo} from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
