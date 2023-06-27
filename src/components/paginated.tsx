218e806194c621dbddd6d035bf49a01bed1bc718
iRichard <zerorichardx@gmail.com>
2023-06-27 12:41:17 -0500

    actual changes

--- src/components/paginated.tsx
+++ src/components/paginated.tsx
@@ -18,7 +18,7 @@ export default function Paginate ({resData, setPaginated}:{resData:blogdata[], s
     
     for(let i = 1 ; i < data+1; i++){
       inded.push(
-        <li className="w-10 h-10 transition-all m-3 relative z-10">
+        <li className="w-10 h-10 transition-all m-3 relative z-10" key={i}>
         <span className={`z-20 w-full h-full flex items-center justify-center absolute top-0 pointer-events-none`}>{i}</span>
         <button className={`w-full h-full absolute rounded-lg top-0 ${actualPage == i?'rotate-0 bg-purple-900':'rotate-45 bg-purple-600'} transition-all z-10 hover:rotate-0 pointer-events-auto`} key={i} value={i} onClick={handleSetActualPageSetPagination}></button>
         </li>)
