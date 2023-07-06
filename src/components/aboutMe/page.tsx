4f39a7008dbcd6b4f0695fed35a6412391ba84a4
iRichard <zerorichardx@gmail.com>
2023-07-05 22:51:25 -0500

    Change in aboutUs

--- src/components/aboutMe/page.tsx
+++ src/components/aboutMe/page.tsx
@@ -4,16 +4,16 @@ export default function AboutMe() {
       <div className='w-[80%] h-5/6 bg-slate-100 flex  rounded-lg overflow-hidden'>
 
         <div className='w-4/6 h-full  flex items-center justify-center'>
-          <div className='w-11/12 h-1/2'>
+          <div className='w-5/6 h-2/3'>
             <div className='w-full h-4/6'>
-              <p className='w-full h-full flex flex-col items-center text-center text-lg font-semibold'>
-                Hola!, mi nombre es Ricardo, mis amigos me suelen llamar 'Richard'.
-                Soy un desarrollador web en crecimiento.<br/>
+              <p className='w-full h-full flex flex-col items-center text-center font-semibold'>
+                Hola!, mi nombre es Ricardo, mis amigos me suelen llamar 'Richard'.<br/>
+                Desarrollador web en crecimiento.<br/>
                 He trabajado en varios proyectos independientes,
-                me encanta aprender cosas nuevas, equivocar, recibir feedback y ponerme a prueba con cada conocimiento que obtengo, como este portafolio/devblog!, 
+                me encanta aprender cosas nuevas, equivocarme, recibir feedback y ponerme a prueba con cada conocimiento que obtengo, como este portafolio/devblog!, 
                 he tenido a mi cargo mas de 4 personas en mas de una ocasion
                 y los resultados han sido excelentes. Actulamente mi meta es encontrarme un lugar en una empresa enfocada en el mundo IT que me de la oportunidad de desarrollarme, pero,
-                igualmente estoy disponible para cualquier proyecto freelance.<br/> Mi objetivo principal es poder costearme mis estudios de ciberseguridad 💜. <br/><p className="text-red-500 animate-bounce delay-700 m-4">Muchas gracias por tu tiempo.</p> 
+                igualmente estoy disponible para cualquier proyecto freelance.<br/> Mi objetivo principal es poder costearme mis estudios de ciberseguridad 💜. <br/><span className="text-red-500 animate-bounce delay-700 m-4 text-xl">Muchas gracias por tu tiempo.</span> 
               </p>
             </div>
             <div className='w-full h-2/6  flex justify-center relative'>
@@ -25,7 +25,7 @@ export default function AboutMe() {
         </div>
         <div className='w-2/6 h-5/6 flex items-center justify-center relative'>
           <div className='h-[50vh] w-5/6 bg-white' />
-          <div className='h-[50vh] w-5/6  border-black border absolute z-10 top-[20vh] right-[3vh]' />
+          <div className='h-[50vh] w-5/6  border-black border absolute z-10 top-[12vh] right-[3vh]' />
         </div>
       </div>
 
