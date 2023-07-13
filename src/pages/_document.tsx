e83dbcdd50dae1b071b24a3dad479869b3414d09
iRichard <zerorichardx@gmail.com>
2023-07-13 15:40:33 -0500

    improve UI design posts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document () {
         <meta name="description" content="Portfolio | blog - I am RichardHD, web developer where I will be publishing my knowledge, discoveries and progress of my day to day as a developer." />
         <meta name="keywords" content="Developer, Portfolio, tecnology, desarrolador, portafolio, tecnologia, web, RichardHD, desarrollador web, RichardHD desarrollador" />
         <meta name="author" content="RichardHD"/>
+        <meta name="og:url" content='../../public/link.png' />
       </Head>
       <body>
         <Main />
--- src/pages/blogs/[postId].tsx
+++ src/pages/blogs/[postId].tsx
@@ -41,7 +41,8 @@ export default function PostPage({ data }: InferGetStaticPropsType<typeof getSta
         <Head>
           <title>{data.frontmatter.title as string}</title>
           <meta name='description' content={`${data.frontmatter.meta ? data.frontmatter.meta : data.frontmatter.title}`} />
-          <meta name='autohr' content='RichardHD'/>
+          <meta name='author' content='RichardHD'/>
+           <meta name='og:description' content={`${data.frontmatter.meta? data.frontmatter.meta:data.frontmatter.title}`} />
         </Head>
         {/* Text & Render MDX */}
         <div className=' w-[90%] sm:w-[70%] min-h-screen inline-grid place-items-center grid-cols-1 m-10 border-2 border-white relative'>
